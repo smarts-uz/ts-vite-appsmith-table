9a4ebc88949c6ffeacf82739d461cfe781ed5994
Ramz001 <rkenjaev1@gmail.com>
2025-11-09 20:00:36 +0500

    Update package version to 0.0.3 and change Vite output format to UMD for better compatibility.

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
       entry: resolve(__dirname, "src/main.tsx"), // now main.tsx
       name: "MyAppsmithComponent",
       fileName: (format) => `app.${format}.js`,
-      formats: ["es"],
+      formats: ["umd"],
     },
   },
   server: {
